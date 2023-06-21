615175310546e07e3cff9ec3db11755c2417566b
tom <101463268+LaSauceBlanche@users.noreply.github.com>
2023-06-21 11:16:13 +0200

    Add files via upload

---
+++ var.js
@@ -0,0 +1,214 @@
+
+
+// menu
+class StickyNavigation {
+    constructor() {
+        this.currentId = null;
+        this.currentTab = null;
+        this.tabContainerHeight = 70;
+        let self = this;
+        $('.et-hero-tab').click(function (event) {
+            self.onTabClick(event, $(this));
+        });
+        $(window).scroll(() => {
+            this.onScroll();
+        });
+        $(window).resize(() => {
+            this.onResize();
+        });
+    }
+
+    onTabClick(event, element) {
+        event.preventDefault();
+        let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
+        $('html, body').animate({ scrollTop: scrollTop }, 600);
+    }
+
+    onScroll() {
+        this.checkTabContainerPosition();
+        this.findCurrentTabSelector();
+    }
+
+    onResize() {
+        if (this.currentId) {
+            this.setSliderCss();
+        }
+    }
+
+    checkTabContainerPosition() {
+        let offset = $('.et-hero-tabs').offset().top + $('.et-hero-tabs').height() - this.tabContainerHeight;
+        if ($(window).scrollTop() > offset) {
+            $('.et-hero-tabs-container').addClass('et-hero-tabs-container--top');
+        } else {
+            $('.et-hero-tabs-container').removeClass('et-hero-tabs-container--top');
+        }
+    }
+
+    findCurrentTabSelector() {
+        let newCurrentId;
+        let newCurrentTab;
+        let self = this;
+        $('.et-hero-tab').each(function () {
+            let id = $(this).attr('href');
+            let offsetTop = $(id).offset().top - self.tabContainerHeight;
+            let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
+            if ($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
+                newCurrentId = id;
+                newCurrentTab = $(this);
+            }
+        });
+        if (this.currentId !== newCurrentId || this.currentId === null) {
+            this.currentId = newCurrentId;
+            this.currentTab = newCurrentTab;
+            this.setSliderCss();
+        }
+    }
+
+    setSliderCss() {
+        let width = 0;
+        let left = 0;
+        if (this.currentTab) {
+            width = this.currentTab.css('width');
+            left = this.currentTab.offset().left;
+        }
+        $('.et-hero-tab-slider').css('width', width);
+        $('.et-hero-tab-slider').css('left', left);
+    }
+}
+
+new StickyNavigation();
+
+
+
+
+
+
+
+// premer slider
+
+
+
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+    if (track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+
+    const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+    track.dataset.percentage = nextPercentage;
+
+    track.animate({
+        transform: `translate(${nextPercentage}%, -50%)`
+    }, { duration: 1200, fill: "forwards" });
+
+    for (const image of track.getElementsByClassName("image")) {
+        image.animate({
+            objectPosition: `${100 + nextPercentage}% center`
+        }, { duration: 1200, fill: "forwards" });
+    }
+}
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+
+
+
+// slide sur le teco des texte
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+
+
+// deuxieme slider d'image 
+
+
+var size = 0;
+var slideNum = 0;
+var xwidth = $('div#slider').width();
+var timerID;
+$('.flex-control-nav.flex-control-paging li').on('click', function () {
+    var index = $('.flex-control-nav.flex-control-paging li').index(this);
+    $('.flex-control-nav.flex-control-paging').find('li a').removeClass('flex-active');
+    $(this).find('a').addClass('flex-active');
+
+    var slideX = xwidth * index;
+    var traStr = 'translate3d(-' + slideX + 'px,0,0)'
+
+    $('.slider-image').css({
+        '-webkit-transform': traStr,
+    });
+
+});
+$(function () {
+    size = $('.flex-control-nav.flex-control-paging li').length;
+    setTimeout('slide()'); 
+});
+
+function slide() {
+    if (slideNum == size) { slideNum = 0; }
+    var slideX = xwidth * slideNum;
+    var traStr = 'translate3d(-' + slideX + 'px,0,0)'
+    $('.flex-control-nav.flex-control-paging').find('li a').removeClass('flex-active');
+    $('.flex-control-nav.flex-control-paging li a').eq(slideNum).addClass('flex-active');
+
+    $('.slider-image').css({
+        '-webkit-transform': traStr,
+    });
+    slideNum++;
+    timerID = setTimeout('slide()', 5000); 
+}
+
+$('#next').on('click', function () {
+    window.clearTimeout(timerID);
+    slide();
+});
+$('#prev').on('click', function () {
+    window.clearTimeout(timerID);
+    if (slideNum > 1) {
+        slideNum = slideNum - 2;
+    } else {
+        slideNum = size - 1;
+    }
+    slide();
+});
+
+
+
