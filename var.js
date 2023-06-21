67490c0211f411ab71afe42ee619a836a2641733
tom <101463268+LaSauceBlanche@users.noreply.github.com>
2023-06-21 16:05:26 +0200

    Add files via upload

--- var.js
+++ var.js
@@ -179,7 +179,7 @@ $('.flex-control-nav.flex-control-paging li').on('click', function () {
 });
 $(function () {
     size = $('.flex-control-nav.flex-control-paging li').length;
-    setTimeout('slide()'); 
+    setTimeout('slide()');
 });
 
 function slide() {
@@ -193,7 +193,7 @@ function slide() {
         '-webkit-transform': traStr,
     });
     slideNum++;
-    timerID = setTimeout('slide()', 5000); 
+    timerID = setTimeout('slide()', 5000);
 }
 
 $('#next').on('click', function () {
@@ -212,3 +212,47 @@ $('#prev').on('click', function () {
 
 
 
+// COPIE LE TEXT
+
+
+
+var btncopy = document.querySelector('.js-copy');
+if (btncopy) {
+    btncopy.addEventListener('click', docopy);
+}
+
+function docopy() {
+
+    // Cible de l'élément qui doit être copié
+    var target = this.dataset.target;
+    var fromElement = document.querySelector(target);
+    if (!fromElement) return;
+
+    // Sélection des caractères concernés
+    var range = document.createRange();
+    var selection = window.getSelection();
+    range.selectNode(fromElement);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    try {
+        // Exécution de la commande de copie
+        var result = document.execCommand('copy');
+        if (result) {
+            // La copie a réussi
+            alert('Copié !');
+        }
+    }
+    catch (err) {
+        // Une erreur est surevnue lors de la tentative de copie
+        alert(err);
+    }
+
+    // Fin de l'opération
+    selection = window.getSelection();
+    if (typeof selection.removeRange === 'function') {
+        selection.removeRange(range);
+    } else if (typeof selection.removeAllRanges === 'function') {
+        selection.removeAllRanges();
+    }
+}
